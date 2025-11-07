5904ebd2b9de95f4a1662b0031457986bb7beda0
Drake Vorndran <37566586+DrakeVorndran@users.noreply.github.com>
2025-11-06 22:41:44 -0500

    fixed cron job

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,12 +1,12 @@
 import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
-  prisma.room.deleteMany({
+  const delCount = await prisma.room.deleteMany({
     where: {
       updatedAt: {
         lt: new Date(Date.now() - 1000 * 60 * 60), // 1 hour ago
       },
     },
   });
-  return Response.json({ status: "Cleanup initiated" });
+  return Response.json({ delCount });
 }
